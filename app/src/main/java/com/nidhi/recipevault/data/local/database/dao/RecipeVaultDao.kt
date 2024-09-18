fba75427dbc4af1937a56d1b9ec405f065286a48
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-09-18 19:57:57 +0530

    Update entity class in dao

--- app/src/main/java/com/nidhi/recipevault/data/local/database/dao/RecipeVaultDao.kt
+++ app/src/main/java/com/nidhi/recipevault/data/local/database/dao/RecipeVaultDao.kt
@@ -4,26 +4,28 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.nidhi.recipevault.data.local.model.IngredientModel
-import com.nidhi.recipevault.data.local.model.RecipeVaultModel
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.IngredientEntity
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.MethodStepEntity
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.RecipeVaultEntity
+
 
 @Dao
 interface RecipeVaultDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertRecipe(recipeVault : RecipeVaultModel)
+    suspend fun insertRecipe(recipeVault : RecipeVaultEntity)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertIngredients(ingredients: List<IngredientModel>)
+    suspend fun insertIngredients(ingredients: List<IngredientEntity>)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMethodSteps(steps : List<String>)
+    suspend fun insertMethodSteps(steps : List<MethodStepEntity>)
 
     @Query("SELECT * FROM recipes where recipe_id = :recipeId")
-    suspend fun getRecipeByID(recipeId : Int) : RecipeVaultModel?
+    suspend fun getRecipeByID(recipeId : Int) : RecipeVaultEntity?
 
     @Query("SELECT * FROM ingredients where recipe_id = :recipeId")
-    suspend fun getIngredientsForRecipe(recipeId: Int) : List<IngredientModel>
+    suspend fun getIngredientsForRecipe(recipeId: Int) : List<IngredientEntity>
 
-    @Query("SELECT step_description FROM method_steps where recipe_id = :recipeId ORDER BY step_order ASC")
-    suspend fun getMethodStepsForRecipe(recipeId: Int) : List<String>
+    @Query("SELECT * FROM method_steps where recipe_id = :recipeId ORDER BY step_order ASC")
+    suspend fun getMethodStepsForRecipe(recipeId: Int) : List<MethodStepEntity>
 }

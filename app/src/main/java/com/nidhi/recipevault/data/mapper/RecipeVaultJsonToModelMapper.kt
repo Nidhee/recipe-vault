d44823b373518c57ed4d6b508ec4f8ae8fe699b9
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-09-18 19:58:18 +0530

    Add json to model mapping function for method step

--- app/src/main/java/com/nidhi/recipevault/data/mapper/RecipeVaultJsonToModelMapper.kt
+++ app/src/main/java/com/nidhi/recipevault/data/mapper/RecipeVaultJsonToModelMapper.kt
@@ -1,13 +1,33 @@
 package com.nidhi.recipevault.data.mapper
 
 import com.google.gson.Gson
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.model.MethodStepModel
 import com.nidhi.recipevault.data.local.model.RecipeVaultModel
 
+/**
+ * Handles json to domain models mapping
+ */
 class RecipeVaultJsonToModelMapper {
+
+    /**
+     * Parses json to domain model classes
+     */
     fun parseJsonToDomainModel(jsonString: String): RecipeVaultModel? {
         val gson = Gson()
         val recipeVault = gson.fromJson(jsonString, RecipeVaultModel::class.java)
         println("recipe vault object $recipeVault")
         return recipeVault
+    }
+    /**
+     * Maps json method string to Method Step model classs
+     */
+    fun mapJsonMethodStepsToDomainModel(recipeId: Int, methodSteps: List<String>): List<MethodStepModel> {
+        return methodSteps.mapIndexed { index, stepDescription ->
+            MethodStepModel(
+                recipeId = recipeId,
+                stepOrder = index + 1,
+                stepDescription = stepDescription
+            )
+        }
     }
 }

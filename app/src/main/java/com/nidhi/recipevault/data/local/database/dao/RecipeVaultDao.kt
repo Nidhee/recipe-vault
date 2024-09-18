3aea929932a69aa21b9103251b4308c366bb77c8
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-09-18 19:16:53 +0530

    Update dao functions according to renamed model classes

--- app/src/main/java/com/nidhi/recipevault/data/local/database/dao/RecipeVaultDao.kt
+++ app/src/main/java/com/nidhi/recipevault/data/local/database/dao/RecipeVaultDao.kt
@@ -4,25 +4,25 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.nidhi.recipevault.data.local.model.Ingredient
-import com.nidhi.recipevault.data.local.model.RecipeVault
+import com.nidhi.recipevault.data.local.model.IngredientModel
+import com.nidhi.recipevault.data.local.model.RecipeVaultModel
 
 @Dao
 interface RecipeVaultDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertRecipe(recipeVault : RecipeVault)
+    suspend fun insertRecipe(recipeVault : RecipeVaultModel)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertIngredients(ingredients: List<Ingredient>)
+    suspend fun insertIngredients(ingredients: List<IngredientModel>)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMethodSteps(steps : List<String>)
 
     @Query("SELECT * FROM recipes where recipe_id = :recipeId")
-    suspend fun getRecipeByID(recipeId : Int) : RecipeVault?
+    suspend fun getRecipeByID(recipeId : Int) : RecipeVaultModel?
 
     @Query("SELECT * FROM ingredients where recipe_id = :recipeId")
-    suspend fun getIngredientsForRecipe(recipeId: Int) : List<Ingredient>
+    suspend fun getIngredientsForRecipe(recipeId: Int) : List<IngredientModel>
 
     @Query("SELECT step_description FROM method_steps where recipe_id = :recipeId ORDER BY step_order ASC")
     suspend fun getMethodStepsForRecipe(recipeId: Int) : List<String>

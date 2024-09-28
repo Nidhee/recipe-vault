558d2b2a6e764f4629bcd6d22314e6d5b67dc897
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-09-28 13:19:21 +0530

    Update RecipeVaultDatabase class to add database annotation with its details

--- app/src/main/java/com/nidhi/recipevault/data/local/database/RecipeVaultDatabase.kt
+++ app/src/main/java/com/nidhi/recipevault/data/local/database/RecipeVaultDatabase.kt
@@ -1,10 +1,23 @@
 package com.nidhi.recipevault.com.nidhi.recipevault.data.local.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.dao.RecipeVaultDao
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.IngredientEntity
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.MethodStepEntity
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.RecipeVaultEntity
 
+
+/**
+ * SQLite Database for storing the recipe database.
+ */
+@Database(
+    entities = [RecipeVaultEntity::class, IngredientEntity::class, MethodStepEntity::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class RecipeVaultDatabase : RoomDatabase() {
-    abstract fun recipeVaultDao() : RecipeVaultDao
+    abstract fun recipeVaultDao(): RecipeVaultDao
 }

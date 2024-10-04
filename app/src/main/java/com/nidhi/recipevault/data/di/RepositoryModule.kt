33849d8fa9ba9c50d00121fdc01f49507ba456b7
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-10-04 23:21:02 +0530

    Create repository module to provide repository instance using hilt

---
+++ app/src/main/java/com/nidhi/recipevault/data/di/RepositoryModule.kt
@@ -0,0 +1,35 @@
+package com.nidhi.recipevault.com.nidhi.recipevault.data.di
+
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.dao.IngredientDao
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.dao.MethodStepDao
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.dao.RecipeVaultDao
+import com.nidhi.recipevault.com.nidhi.recipevault.data.mapper.RecipeVaultModelToEntityMapper
+import com.nidhi.recipevault.com.nidhi.recipevault.data.repository.RecipeRepositoryImpl
+import com.nidhi.recipevault.com.nidhi.recipevault.domain.repository.RecipeRepository
+import com.nidhi.recipevault.data.local.json.RecipeVaultJsonDataSource
+import com.nidhi.recipevault.data.mapper.RecipeVaultJsonToModelMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideRecipeRepository(
+        recipeVaultDao: RecipeVaultDao,
+        methodStepDao: MethodStepDao,
+        ingredientDao: IngredientDao,
+        recipeVaultModelToEntityMapper: RecipeVaultModelToEntityMapper
+    ): RecipeRepository {
+        return RecipeRepositoryImpl(
+            recipeVaultDao,
+            methodStepDao,
+            ingredientDao,
+            recipeVaultModelToEntityMapper
+        )
+    }
+}

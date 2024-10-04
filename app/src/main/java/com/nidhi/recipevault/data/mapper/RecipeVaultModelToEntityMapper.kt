f17e48a968442813e2c7f7bf948a0cd7267a9340
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-10-04 20:36:45 +0530

    Create recipe models to entities mapper functions

--- app/src/main/java/com/nidhi/recipevault/data/mapper/RecipeVaultModelToEntityMapper.kt
+++ app/src/main/java/com/nidhi/recipevault/data/mapper/RecipeVaultModelToEntityMapper.kt
@@ -1,7 +1,14 @@
 package com.nidhi.recipevault.com.nidhi.recipevault.data.mapper
 
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.Cuisine
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.DifficultyLevel
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.MealType
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.RecipeUnit
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.IngredientEntity
 import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.MethodStepEntity
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.RecipeVaultEntity
 import com.nidhi.recipevault.com.nidhi.recipevault.data.local.model.MethodStepModel
+import com.nidhi.recipevault.data.local.model.RecipeVaultItemModel
 
 /**
  *  Handles domain models to entity and entity to domain models mapping
@@ -9,6 +16,7 @@ import com.nidhi.recipevault.com.nidhi.recipevault.data.local.model.MethodStepMo
 class RecipeVaultModelToEntityMapper {
 
     /**
+     * TODO : check this mapper is needed or not
      *  Maps method step model to method step entity
      */
     fun mapToMethodStepEntity(methodStepModel: MethodStepModel): MethodStepEntity {
@@ -19,6 +27,7 @@ class RecipeVaultModelToEntityMapper {
         )
     }
     /**
+     * TODO : check this mapper is needed or not
      *  Maps method step entity to method step model
      */
     fun mapToMethodStepModel(methodStepEntity: MethodStepEntity): MethodStepModel {
@@ -27,5 +36,93 @@ class RecipeVaultModelToEntityMapper {
             stepOrder = methodStepEntity.stepOrder,
             stepDescription = methodStepEntity.stepDescription
         )
+    }
+
+    /**
+     Mapping from domain model (RecipeVaultItemModel) to entity (RecipeEntity)
+     */
+    fun mapRecipeVaultItemModelToEntity(recipeModel: RecipeVaultItemModel): RecipeVaultEntity {
+        return RecipeVaultEntity(
+            recipeId = recipeModel.recipeId,
+            name = recipeModel.name,
+            description = recipeModel.description,
+            prepTimeMinutes = recipeModel.prepTimeMinutes,
+            cookTimeMinutes = recipeModel.cookTimeMinutes,
+            servings = recipeModel.servings,
+            difficultyLevel = stringToDifficultyLevel(recipeModel.difficultyLevel),
+            mealType = stringToMealType(recipeModel.mealType),
+            cuisine = stringToCuisine(recipeModel.cuisine),
+            thumbnail = recipeModel.thumbnail
+        )
+    }
+
+    /**
+     * Map from Method (List<String>) to MethodStepEntity
+     */
+    fun mapMethodStepModelToEntity(recipeModel: RecipeVaultItemModel): List<MethodStepEntity> {
+        return recipeModel.method.mapIndexed { index, step ->
+            MethodStepEntity(
+                recipeId = recipeModel.recipeId,
+                stepOrder = index + 1,
+                stepDescription = step
+            )
+        }
+    }
+    /**
+     * Map from Ingredient (List<String>)  to IngredientEntity
+      */
+    fun mapIngredientModelToEntity(recipeModel: RecipeVaultItemModel): List<IngredientEntity> {
+        return recipeModel.ingredients.map { ingredientModel ->
+            IngredientEntity(
+                recipeId = recipeModel.recipeId,
+                item = ingredientModel.item,
+                qty = ingredientModel.qty,
+                maxQty = ingredientModel.maxQty,  // Assuming you have a `maxQuantity` in your model
+                unit = stringToRecipeUnit(ingredientModel.unit)  // Assuming the unit is handled as an enum
+            )
+        }
+    }
+    /**
+     * Method to convert String to DifficultyLevel enum
+      */
+    private fun stringToDifficultyLevel(value: String): DifficultyLevel {
+        return try {
+            DifficultyLevel.valueOf(value.uppercase()) // Handle case-sensitivity
+        } catch (e: IllegalArgumentException) {
+            DifficultyLevel.EASY // Fallback to a default value
+        }
+    }
+
+    /**
+     * Method to convert String to MealType enum
+     */
+    private fun stringToMealType(value: String): MealType {
+        return try {
+            MealType.valueOf(value.uppercase())
+        } catch (e: IllegalArgumentException) {
+            MealType.DRINK // Fallback to a default value
+        }
+    }
+
+    /**
+     *   Method to convert String to Cuisine enum
+      */
+    private fun stringToCuisine(value: String): Cuisine {
+        return try {
+            Cuisine.valueOf(value.uppercase())
+        } catch (e: IllegalArgumentException) {
+            Cuisine.INDIAN // Fallback to a default value
+        }
+    }
+
+    /**
+     *   Method to convert String to recipe unit enum
+     */
+    private fun stringToRecipeUnit(value: String): RecipeUnit {
+        return try {
+            RecipeUnit.valueOf(value.uppercase())
+        } catch (e: IllegalArgumentException) {
+            RecipeUnit.NOS // Fallback to a default value
+        }
     }
 }

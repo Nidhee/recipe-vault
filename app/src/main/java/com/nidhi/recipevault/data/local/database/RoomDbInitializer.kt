734d5bcceb9f207e42a66796a578676afe42dfa6
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-10-04 23:17:40 +0530

    Create Room database initialiser class to pre-populate database from local json in assets

---
+++ app/src/main/java/com/nidhi/recipevault/data/local/database/RoomDbInitializer.kt
@@ -0,0 +1,49 @@
+package com.nidhi.recipevault.com.nidhi.recipevault.data.local.database
+
+import android.util.Log
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.dao.IngredientDao
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.dao.MethodStepDao
+import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.dao.RecipeVaultDao
+import com.nidhi.recipevault.com.nidhi.recipevault.data.mapper.RecipeVaultModelToEntityMapper
+import com.nidhi.recipevault.com.nidhi.recipevault.utils.LogUtils
+import com.nidhi.recipevault.data.local.json.RecipeVaultJsonDataSource
+import com.nidhi.recipevault.data.mapper.RecipeVaultJsonToModelMapper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Provider
+
+class RoomDbInitializer (
+    private val recipeVaultDao: Provider<RecipeVaultDao>,
+    private val methodStepDao:  Provider<MethodStepDao>,
+    private val ingredientDao:  Provider<IngredientDao>,
+    private val recipeVaultJsonDataSource: Provider<RecipeVaultJsonDataSource>,
+    private val recipeVaultJsonToModelMapper: Provider<RecipeVaultJsonToModelMapper>,
+    private val recipeVaultModelToEntityMapper: Provider<RecipeVaultModelToEntityMapper>,
+): RoomDatabase.Callback() {
+    override fun onCreate(db: SupportSQLiteDatabase) {
+        super.onCreate(db)
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.d(LogUtils.getTag(this::class.java), "room database callback >> RoomDbInitializer")
+            // Read the JSON from RecipeVaultJsonDataSource
+            val recipes = recipeVaultJsonDataSource.get().readJsonFromAssets("RecipeVaultJSON.json")
+            // parse json string to model
+            val recipeVaultModel = recipeVaultJsonToModelMapper.get().parseJsonToDomainModel(recipes)
+
+            // Insert data models into Room database
+            recipeVaultModel?.forEach { recipe ->
+                recipeVaultDao.get().insertRecipe(
+                    recipeVaultModelToEntityMapper.get().mapRecipeVaultItemModelToEntity(recipeModel = recipe)
+                )
+                ingredientDao.get().insertIngredients(
+                    recipeVaultModelToEntityMapper.get().mapIngredientModelToEntity(recipeModel = recipe)
+                )
+                methodStepDao.get().insertMethodSteps(
+                    recipeVaultModelToEntityMapper.get().mapMethodStepModelToEntity(recipeModel = recipe)
+                )
+            }
+        }
+    }
+}

e2f1cbeb3b50a386360f30e9740427e9ae4444ed
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-10-04 10:45:17 +0530

    Add log utils helper

---
+++ app/src/main/java/com/nidhi/recipevault/utils/LogUtils.kt
@@ -0,0 +1,9 @@
+package com.nidhi.recipevault.com.nidhi.recipevault.utils
+
+object LogUtils {
+    private const val COMMON_TAG = "RecipeApp"
+
+    fun getTag(cls: Class<*>): String {
+        return "$COMMON_TAG${cls.simpleName}:"
+    }
+}

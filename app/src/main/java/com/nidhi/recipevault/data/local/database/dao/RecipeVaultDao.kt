515c3ffeafdafdfc71a5190f75377ffb223e7d3b
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-10-04 19:38:58 +0530

    Remove Ingredients and Method step DAO methods from this DAO, Add get all recipe method in this DAO

--- app/src/main/java/com/nidhi/recipevault/data/local/database/dao/RecipeVaultDao.kt
+++ app/src/main/java/com/nidhi/recipevault/data/local/database/dao/RecipeVaultDao.kt
@@ -7,6 +7,7 @@ import androidx.room.Query
 import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.IngredientEntity
 import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.MethodStepEntity
 import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.RecipeVaultEntity
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
@@ -14,18 +15,8 @@ interface RecipeVaultDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertRecipe(recipeVault : RecipeVaultEntity)
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertIngredients(ingredients: List<IngredientEntity>)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMethodSteps(steps : List<MethodStepEntity>)
-
     @Query("SELECT * FROM recipes where recipe_id = :recipeId")
     suspend fun getRecipeByID(recipeId : Int) : RecipeVaultEntity?
-
-    @Query("SELECT * FROM ingredients where recipe_id = :recipeId")
-    suspend fun getIngredientsForRecipe(recipeId: Int) : List<IngredientEntity>
-
-    @Query("SELECT * FROM method_steps where recipe_id = :recipeId ORDER BY step_order ASC")
-    suspend fun getMethodStepsForRecipe(recipeId: Int) : List<MethodStepEntity>
+    @Query("SELECT * FROM recipes")
+    fun getAllRecipes() : Flow<List<RecipeVaultEntity>>
 }

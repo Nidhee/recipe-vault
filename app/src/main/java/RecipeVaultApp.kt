b8a9377763eab5fe570926807ed1d47c123ea1ca
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-10-04 10:50:10 +0530

    Add logs on application class creation

--- app/src/main/java/RecipeVaultApp.kt
+++ app/src/main/java/RecipeVaultApp.kt
@@ -1,6 +1,15 @@
 package com.nidhi.recipevault
 import android.app.Application
+import android.util.Log
+import com.nidhi.recipevault.com.nidhi.recipevault.utils.LogUtils
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class RecipeVaultApp : Application() {}
+class RecipeVaultApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(LogUtils.getTag(this::class.java), "Application class is creating")
+    }
+}

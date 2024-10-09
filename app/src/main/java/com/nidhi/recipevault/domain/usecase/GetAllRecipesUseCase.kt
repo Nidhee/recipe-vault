2ef88eb8d7d99c6816b59389b08b598f96ce4817
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-10-09 19:29:57 +0530

    Update GetAllRecipesUseCase to return list of recipe model

--- app/src/main/java/com/nidhi/recipevault/domain/usecase/GetAllRecipesUseCase.kt
+++ app/src/main/java/com/nidhi/recipevault/domain/usecase/GetAllRecipesUseCase.kt
@@ -1,13 +1,12 @@
 package com.nidhi.recipevault.com.nidhi.recipevault.domain.usecase
 
-import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.entity.RecipeVaultEntity
 import com.nidhi.recipevault.com.nidhi.recipevault.domain.repository.RecipeRepository
-import com.nidhi.recipevault.data.local.model.RecipeVaultModel
+import com.nidhi.recipevault.domain.model.Recipe
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetAllRecipesUseCase @Inject constructor(private val recipeRepository: RecipeRepository) {
-    operator fun invoke(): Flow<List<RecipeVaultEntity>> {
+    operator fun invoke(): Flow<List<Recipe>> {
         return recipeRepository.getAllRecipes()
     }
 }

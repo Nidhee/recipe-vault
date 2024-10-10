1493531483d66071f40b4eba7c7515d78705cd9c
Nidhi Gondhia <nidhigondhia@gmail.com>
2024-10-10 17:24:20 +0530

    Update RoomDbInitializer to handle exceptions while reading and parsing json. Pass error state using database error state.

--- app/src/main/java/com/nidhi/recipevault/data/local/database/RoomDbInitializer.kt
+++ app/src/main/java/com/nidhi/recipevault/data/local/database/RoomDbInitializer.kt
@@ -3,6 +3,7 @@ package com.nidhi.recipevault.com.nidhi.recipevault.data.local.database
 import android.util.Log
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.google.gson.JsonSyntaxException
 import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.dao.IngredientDao
 import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.dao.MethodStepDao
 import com.nidhi.recipevault.com.nidhi.recipevault.data.local.database.dao.RecipeVaultDao
@@ -26,23 +27,39 @@ class RoomDbInitializer (
     override fun onCreate(db: SupportSQLiteDatabase) {
         super.onCreate(db)
         CoroutineScope(Dispatchers.IO).launch {
-            Log.d(LogUtils.getTag(this::class.java), "room database callback >> RoomDbInitializer")
-            // Read the JSON from RecipeVaultJsonDataSource
-            val recipes = recipeVaultJsonDataSource.get().readJsonFromAssets("RecipeVaultJSON.json")
-            // parse json string to model
-            val recipeVaultModel = recipeVaultJsonToModelMapper.get().parseJsonToDomainModel(recipes)
+            try {
+                Log.d(LogUtils.getTag(this::class.java), "room database callback >> RoomDbInitializer")
+                // Read the JSON from RecipeVaultJsonDataSource
+                val recipes = recipeVaultJsonDataSource.get().readJsonFromAssets("RecipeVaultJSON.json")
+                // parse json string to model
+                val recipeVaultModel = recipeVaultJsonToModelMapper.get().parseJsonToDomainModel(recipes)
 
-            // Insert data models into Room database
-            recipeVaultModel?.forEach { recipe ->
-                recipeVaultDao.get().insertRecipe(
-                    recipeVaultModelToEntityMapper.get().mapRecipeVaultItemModelToEntity(recipeModel = recipe)
-                )
-                ingredientDao.get().insertIngredients(
-                    recipeVaultModelToEntityMapper.get().mapIngredientModelToEntity(recipeModel = recipe)
-                )
-                methodStepDao.get().insertMethodSteps(
-                    recipeVaultModelToEntityMapper.get().mapMethodStepModelToEntity(recipeModel = recipe)
-                )
+                if (recipeVaultModel.isNullOrEmpty()) {
+                    Log.e(LogUtils.getTag(this::class.java), "No valid recipes found in JSON")
+                }
+                // Insert data models into Room database
+                recipeVaultModel?.forEach { recipe ->
+                    recipeVaultDao.get().insertRecipe(
+                        recipeVaultModelToEntityMapper.get()
+                            .mapRecipeVaultItemModelToEntity(recipeModel = recipe)
+                    )
+                    ingredientDao.get().insertIngredients(
+                        recipeVaultModelToEntityMapper.get()
+                            .mapIngredientModelToEntity(recipeModel = recipe)
+                    )
+                    methodStepDao.get().insertMethodSteps(
+                        recipeVaultModelToEntityMapper.get()
+                            .mapMethodStepModelToEntity(recipeModel = recipe)
+                    )
+                }
+            }catch (e: JsonSyntaxException) {
+                Log.e(LogUtils.getTag(this::class.java), "room database callback >> RoomDbInitializer >> JSON parsing error\n$e")
+                // Update the error state
+                DatabaseErrorStateHolder.updateErrorState(e)
+            } catch (e: Exception) {
+                Log.e(LogUtils.getTag(this::class.java), "room database callback >> RoomDbInitializer >> Unknown error occurred\n$e")
+                // Update the error state
+                DatabaseErrorStateHolder.updateErrorState(e)
             }
         }
     }

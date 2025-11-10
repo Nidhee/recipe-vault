069b771ff9f69ee6583fabfb613a2d08b9bad050
Nidhi Gondhia <nidhigondhia@gmail.com>
2025-11-10 13:22:28 +0530

    Fix import issue in Repository module

--- app/src/main/java/com/nidhi/recipevault/data/di/RepositoryModule.kt
+++ app/src/main/java/com/nidhi/recipevault/data/di/RepositoryModule.kt
@@ -3,9 +3,9 @@ package com.nidhi.recipevault.data.di
 import com.nidhi.recipevault.data.local.database.dao.IngredientDao
 import com.nidhi.recipevault.data.local.database.dao.MethodStepDao
 import com.nidhi.recipevault.data.local.database.dao.RecipeVaultDao
+import com.nidhi.recipevault.data.mapper.RecipeMapper
 import com.nidhi.recipevault.data.mapper.RecipeVaultModelToEntityMapper
 import com.nidhi.recipevault.data.repository.RecipeRepositoryImpl
-import com.nidhi.recipevault.com.nidhi.recipevault.data.mapper.RecipeMapper
 import com.nidhi.recipevault.domain.repository.RecipeRepository
 import dagger.Module
 import dagger.Provides
